bbf36a3576ad0dcf59f7f0c0801d2be1016eb1dc
banesingh.pachlaniya <banesingh.pachlaniya@infobeans.com>
2023-09-24 17:15:16 +0530

    terraform apply 10

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,8 +1,8 @@
 # aws --version
 # aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# arn:aws:s3:::aws-kubernetes-cluster-iaac-pipeline-tfstate
-# AKIAYWNOFTJDFS5CIOAV
+# terraform-backend-state-in28minutes-123
+# AKIA4AHVNOD7OOO6T4KI
 
 
 terraform {
@@ -21,59 +21,72 @@ data "aws_subnet_ids" "subnets" {
   vpc_id = aws_default_vpc.default.id
 }
 
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 2.12"
+  //>>Uncomment this section once EKS is created - Start
+  # host                   = data.aws_eks_cluster.cluster.endpoint #module.in28minutes-cluster.cluster_endpoint
+  # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  # token                  = data.aws_eks_cluster_auth.cluster.token
+  //>>Uncomment this section once EKS is created - End
 }
 
 module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
-  cluster_version = "1.14"
-  subnets = ["subnet-0db5d4bc61b3bcdfc", "subnet-0837d9445dd49afea"]
-  #subnets = data.aws_subnet_ids.subnets.ids
+  cluster_version = "1.23"
+  subnet_ids         = ["subnet-0db5d4bc61b3bcdfc", "subnet-0837d9445dd49afea", "subnet-0b3a189576b707cd7"] #CHANGE # Donot choose subnet from us-east-1e
   vpc_id          = aws_default_vpc.default.id
-  #vpc_id         = "vpc-020a4fecf416cdf2d"
 
-  node_groups = [
-    {
-      instance_type = "t2.micro"
-      max_capacity  = 5
-      desired_capacity = 2
-      min_capacity  = 2
+  //Newly added entry to allow connection to the api server
+  //Without this change error in step 163 in course will not go away
+  cluster_endpoint_public_access  = true
+
+# EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t2.small", "t2.medium"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t2.medium"]
     }
-  ]
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
-}
-
-
-# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
-# ServiceAccount needs permissions to create deployments 
-# and services in default namespace
-resource "kubernetes_cluster_role_binding" "example" {
-  metadata {
-    name = "fabric8-rbac"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "default"
-    namespace = "default"
   }
 }
+
+//>>Uncomment this section once EKS is created - Start
+#  data "aws_eks_cluster" "cluster" {
+#    name = "in28minutes-cluster" #module.in28minutes-cluster.cluster_name
+#  }
+
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = "in28minutes-cluster" #module.in28minutes-cluster.cluster_name
+# }
+
+
+# # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+# # ServiceAccount needs permissions to create deployments 
+# # and services in default namespace
+# resource "kubernetes_cluster_role_binding" "example" {
+#   metadata {
+#     name = "fabric8-rbac"
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "default"
+#     namespace = "default"
+#   }
+# }
+//>>Uncomment this section once EKS is created - End
 
 # Needed to set the default region
 provider "aws" {
